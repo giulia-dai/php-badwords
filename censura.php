29fd1919ed899e6ec0e08913c5daef9aff7b1263
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-04-18 10:20:32 +0200

    piccola modifica

--- censura.php
+++ censura.php
@@ -2,28 +2,27 @@
 $testo= $_GET['testo'];
 $censura=$_GET['censorship'];
 
-$parola_nascosta = str_replace($censura,' *** ',$testo);
+$parola_nascosta = str_replace($censura,"***",$testo);
 $lunghezza_censura= strlen($parola_nascosta);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
+ <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bad Words</title>
-</head>
-<body>
+ </head>
+ <body>
 
-<p> <?php echo $testo ?> </p>
-<h4><?php echo 'il testo è lungo' . ' ' . strlen($testo) . ' ' . 'caratteri.' ?></h4>
+   <p> <?php echo $testo ?> </p>
+   <h4><?php echo 'il testo è lungo' . ' ' . strlen($testo) . ' ' . 'caratteri.' ?></h4>
 
-<h3>Nuovo testo con censure</h3>
-<p><?php echo $parola_nascosta ?></p>
-<h4><?php echo 'il testo è lungo' . ' ' . $lunghezza_censura . ' ' . 'caratteri.' ?></h4>
+   <h3>Nuovo testo con censure</h3>
+   <p><?php echo $parola_nascosta ?></p>
+   <h4>il nuovo testo è lungo <?php echo $lunghezza_censura?> caratteri</h4>
 
-    
-</body>
+ </body>
 </html>
